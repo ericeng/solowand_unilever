e9bef99feddb92dff31ff0ffd404b98538a4caa3
ericeng <eric@solomomo.com>
2019-02-19 08:21:06 -0800

    add the requirement of specifying the server IP address on the command line

--- socket_client.c
+++ socket_client.c
@@ -15,6 +15,13 @@ int main(int argc, char const *argv[])
 	struct sockaddr_in serv_addr; 
 	char *hello = "Hello from client"; 
 	char buffer[1024] = {0}; 
+
+	if ( argc != 2 )
+        {
+		printf("\n Must specify server IP address.\n");
+		return -1;
+        }
+
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
 	{ 
 		printf("\n Socket creation error \n"); 
@@ -27,7 +34,7 @@ int main(int argc, char const *argv[])
 	serv_addr.sin_port = htons(PORT); 
 	
 	// Convert IPv4 and IPv6 addresses from text to binary form 
-	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0) 
 	{ 
 		printf("\nInvalid address/ Address not supported \n"); 
 		return -1; 
