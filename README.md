27ed1d1cfe2f89aea665d7394f3215c0b6aa7cac
ericeng <eric@engstrom.tv>
2019-09-25 13:25:22 -0400

    changes to make wand_sim work

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
 # solowand_unilever
 
-to get the appropriate demo programs to run,
+you must install streameye
+
+########
+
+cd ~
+sudo git clone https://github.com/ccrisan/streameye
+cd ~/streameye
+sudo make
+sudo make install
+cd ~
+
+########
+
+# to get the appropriate demo programs to run,
+# you can edit the Makefile to set the -D to __DEMO__, __TEST__, or nothing for
+# production which isn't done at this time
 
 sudo cp solo*.service /lib/systemd/system
 cd /lib/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable solowand.service
-sudo systemctl daemon-reload
 sudo systemctl enable soloeye.service
+sudo systemctl daemon-reload
 
-and then reboot to test.  it should be 10 seconds after boot before streameye shows up in the ps -A list.  wand should be there as soon as you can run ps -A
+#and either solowand.service for the real wand or
+sudo systemctl enable solowand.service
+# or this for the simulated wand
+sudo systemctl enable solosim.service
+
+# and then reboot to test.  it should be 10 seconds after boot before streameye
+# shows up in the ps -A list.  wand should be there as soon as you can run ps -A
+
 
--- wand_sim.c
+++ wand_sim.c
@@ -1,8 +1,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <wiringPi.h>
 #include <signal.h>
+
+#define POWER_BLINK_DELAY_ON	20
+#define POWER_BLINK_DELAY_OFF	20
 
 #define GPIO_LED        21
 #define GPIO_BUTTON_0   26
@@ -10,8 +14,10 @@
 #define GPIO_RED        19
 #define GPIO_GREEN      16
 #define GPIO_BLUE       13
-#define GPIO_CRADLE_0   04
-#define GPIO_CRADLE_1   18
+#define GPIO_CRADLE_0   17
+#define GPIO_CRADLE_1   27
+#define GPIO_CHARGING_0 04
+#define GPIO_CHARGING_1 18
 
 void sigintHandler( int sig_num )
   {
@@ -21,6 +27,7 @@ void sigintHandler( int sig_num )
   digitalWrite( GPIO_GREEN, 0 );
   digitalWrite( GPIO_BLUE, 0 );
   digitalWrite( GPIO_CRADLE_0, 0 );
+  digitalWrite( GPIO_CHARGING_0, 0 );
 
   printf( "Wand Simulator Exiting with all GPIOs cleared.\n" );
 
@@ -31,6 +38,9 @@ void sigintHandler( int sig_num )
 
 int main( int argc, char** argv )
   {
+  int on = POWER_BLINK_DELAY_ON;
+  int off = POWER_BLINK_DELAY_OFF;
+
   signal( SIGINT, sigintHandler );
 
   wiringPiSetupGpio();
@@ -43,35 +53,60 @@ int main( int argc, char** argv )
   pinMode( GPIO_BLUE, OUTPUT );
   pinMode( GPIO_CRADLE_0, OUTPUT );
   pinMode( GPIO_CRADLE_1, INPUT );
+  pinMode( GPIO_CHARGING_0, OUTPUT );
+  pinMode( GPIO_CHARGING_1, INPUT );
 
   printf( "Wand Simulator\n");
 
   digitalWrite( GPIO_BUTTON_0, 1 );
   digitalWrite( GPIO_CRADLE_0, 1 );
+  digitalWrite( GPIO_CHARGING_0, 1 );
 
   while( 1 )
     {
     while( !digitalRead( GPIO_BUTTON_1 ))
       {
+      digitalWrite( GPIO_GREEN, 0 );
       digitalWrite( GPIO_LED, 1 );
+      sleep( 1 );
       }
     digitalWrite( GPIO_LED, 0 );
 
-    digitalWrite( GPIO_RED, 1 );
-    delay( 500 );
-    digitalWrite( GPIO_RED, 0 );
-    delay( 500 );
-    digitalWrite( GPIO_GREEN, 1 );
-    delay( 500 );
-    digitalWrite( GPIO_GREEN, 0 );
-    delay( 500 );
-
     while( digitalRead( GPIO_CRADLE_1 ))
       {
+      digitalWrite( GPIO_GREEN, 0 );
       digitalWrite( GPIO_BLUE, 1 );
       delay( 50 );
       digitalWrite( GPIO_BLUE, 0 );
       delay( 50 );
+      }
+
+    while( digitalRead( GPIO_CHARGING_1 ))
+      {
+      digitalWrite( GPIO_GREEN, 0 );
+      digitalWrite( GPIO_RED, 1 );
+      delay( 50 );
+      digitalWrite( GPIO_RED, 0 );
+      delay( 50 );
+      }
+
+    sleep( 1 );
+    if( on )
+      {
+      digitalWrite( GPIO_GREEN, 1 );
+      delay( 50 );
+      on--;
+      }
+    else if( off )
+      {
+      digitalWrite( GPIO_GREEN, 0 );
+      delay( 50 );
+      off--;
+      }
+    else
+      {
+      on = POWER_BLINK_DELAY_ON;
+      off = POWER_BLINK_DELAY_OFF;
       }
     }
 
